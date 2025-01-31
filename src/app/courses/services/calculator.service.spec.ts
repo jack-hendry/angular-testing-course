20a8031184559d65b56bbfdbdbd01de7ed9d0344
jack-hendry <jack.r.hendry.p@gmail.com>
2025-01-31 12:03:21 -0500

    Lesson 8): Structuring Angular Unit Tests - Test Setup using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,28 +1,29 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
-describe('CalculatorService', () => {
+describe("CalculatorService", () => {
 
-    it('should add two numbers', () => {
-        
-        const logger = jasmine.createSpyObj('LoggerService', ['log']); // create a spy object
+  let caclulator: CalculatorService,loggerSpy:any;
+  beforeEach(() => {
+    console.log("Calling beforeEach");
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]); // create a spy object
 
-        const caclulator= new CalculatorService(logger);// setup phase
+    caclulator = new CalculatorService(loggerSpy); // setup phase
+  });
 
-        const result = caclulator.add(2,2);//execution phase
+  it("should add two numbers", () => {
+    console.log("Add test");
+    const result = caclulator.add(2, 2); //execution phase
 
-        expect(result).toBe(4); // test phase
-        expect(logger.log).toHaveBeenCalledTimes(1);
-    })
+    expect(result).toBe(4); // test phase
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
 
-    it('should subtract two numbers', () => {
+  it("should subtract two numbers", () => {
+    console.log("Subtract test");
+    const result = caclulator.subtract(2, 2); //execution phase
 
-        const caclulator= new CalculatorService(new LoggerService());// setup phase
-
-        const result = caclulator.subtract(2,2); //execution phase
-
-        expect(result).toBe(0, "unexpected subtraction result"); // test phase
-        
-    })
-
+    expect(result).toBe(0, "unexpected subtraction result"); // test phase
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
 });
