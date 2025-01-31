412146910cdb430773b0c2abab425c10c59427ae
jack-hendry <jack.r.hendry.p@gmail.com>
2025-01-31 11:04:50 -0500

    Lesson 6): implemntation of 1st Jasmine specification

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,13 +1,25 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe('CalculatorService', () => {
 
     it('should add two numbers', () => {
-        pending(); // not ready for execution
+        
+        const caclulator= new CalculatorService(new LoggerService());// setup phase
 
+        const result = caclulator.add(2,2);//execution phase
+
+        expect(result).toBe(4); // test phase
     })
 
     it('should subtract two numbers', () => {
 
-        fail();
+        const caclulator= new CalculatorService(new LoggerService());// setup phase
+
+        const result = caclulator.subtract(2,2); //execution phase
+
+        expect(result).toBe(0, "unexpected subtraction result"); // test phase
+        
     })
 
 });
