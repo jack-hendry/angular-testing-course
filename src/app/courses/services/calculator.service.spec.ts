4138935a9679e8544f72d38aa82bf459e919abc2
jack-hendry <jack.r.hendry.p@gmail.com>
2025-01-31 11:41:51 -0500

    Lesson 7): Intro Jasmine Spies

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -5,11 +5,14 @@ describe('CalculatorService', () => {
 
     it('should add two numbers', () => {
         
-        const caclulator= new CalculatorService(new LoggerService());// setup phase
+        const logger = jasmine.createSpyObj('LoggerService', ['log']); // create a spy object
+
+        const caclulator= new CalculatorService(logger);// setup phase
 
         const result = caclulator.add(2,2);//execution phase
 
         expect(result).toBe(4); // test phase
+        expect(logger.log).toHaveBeenCalledTimes(1);
     })
 
     it('should subtract two numbers', () => {
