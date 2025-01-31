b73fe04313b0fd6f9f3a0db34d4d252540acf632
jack-hendry <jack.r.hendry.p@gmail.com>
2025-01-31 10:47:38 -0500

    Lesson 5): quick intro to jasmine

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,13 @@
+describe('CalculatorService', () => {
+
+    it('should add two numbers', () => {
+        pending(); // not ready for execution
+
+    })
+
+    it('should subtract two numbers', () => {
+
+        fail();
+    })
+
+});
